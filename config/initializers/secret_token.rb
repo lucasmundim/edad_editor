9db741fd7f31d37beee8344260dfb334d5df640f
Lucas Mundim <lucas.mundim@corp.globo.com>
2011-04-12 23:47:28 -0300

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdadEditor::Application.config.secret_token = '7e9edd54386a7a8d8a202e557890ad9945b0e24316948469402852d70f4bc9767085317f4819faf95cf02abb566b4aef6f07c1727b15e8d53eaa449d1e1f3e87'
